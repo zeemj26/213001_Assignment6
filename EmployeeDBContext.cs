db5492a6257c80f4f22a6ad539a6499f9153492f
Zahra M <105454060+zeemj26@users.noreply.github.com>
2024-01-13 14:24:48 +0500

    Add files via upload

---
+++ EmployeeDBContext.cs
@@ -0,0 +1,22 @@
+﻿using _213002_Assignment06_Act2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _213002_Assignment06_Act2
+{
+    internal class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=EFConsole;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
